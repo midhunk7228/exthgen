1172dada74e040e076c41eeaa6a3239bb120817e
dilshadbasith <dilshadbasith01@gmail.com>
2025-03-04 17:38:24 +0530

    gave scroll

--- src/pages/Blogs/BlogDetails.tsx
+++ src/pages/Blogs/BlogDetails.tsx
@@ -3,12 +3,24 @@ import BlogDetailsContent from '../../components/BlogDetailsContent'
 import BlogDetailsHeader from '../../components/BlogDetailsHeader'
 import BuyMeACoffee from '../../components/BuyMeACoffee'
 import RelatedBlogs from '../../components/RelatedBlogs'
+import { useEffect } from 'react';
 
 function BlogsDetails() {
   const location = useLocation();
 
   const blogDetails = location.state?.blog;
   console.log(blogDetails, "employeedetails")
+
+  useEffect(() => {
+    if ('scrollBehavior' in document.documentElement.style) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [location.pathname]);
 
   return (
     <div className="flex flex-col">
