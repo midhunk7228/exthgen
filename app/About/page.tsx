0bad2b383c19f85b987bafbc056dd2620bffd6bb
midhunk7228 <midkallanorr@gmail.com>
2025-06-17 01:53:45 +0530

    test2

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -12,7 +12,7 @@ function About() {
         <InnovationSection />
       </div>
       <TeamPage />
-      {/* <WhyWe /> */}
+      <WhyWe />
       {/* <BuyMeACoffee url="./footer-vid.mp4" /> */}
     </div>
   );
